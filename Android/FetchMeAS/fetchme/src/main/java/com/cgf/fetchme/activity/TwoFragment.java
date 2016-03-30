3ed393ce3b6f30b66337630906c0326336306c27
chenguanfu511 <chenguanfu511@gmail.com>
2016-03-30 15:45:21 +0800

    merge the tab view and the drawer view together with the main project fetchme

---
+++ Android/FetchMeAS/fetchme/src/main/java/com/cgf/fetchme/activity/TwoFragment.java
@@ -0,0 +1,28 @@
+package com.cgf.fetchme.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cgf.fetchme.R;
+
+/**
+ * Created by chenguanfu on 3/30/2016.
+ */
+public class TwoFragment extends Fragment {
+    public TwoFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
