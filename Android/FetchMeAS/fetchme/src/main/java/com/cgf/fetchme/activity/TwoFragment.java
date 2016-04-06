6f1b6d61c8ea2c68e93719af0ee719b709c05619
chenguanfu511 <chenguanfu511@gmail.com>
2016-04-06 14:45:13 +0800

    add the card view for the two fragment and merge with the fetchme app

--- Android/FetchMeAS/fetchme/src/main/java/com/cgf/fetchme/activity/TwoFragment.java
+++ Android/FetchMeAS/fetchme/src/main/java/com/cgf/fetchme/activity/TwoFragment.java
@@ -2,16 +2,26 @@ package com.cgf.fetchme.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cgf.fetchme.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by chenguanfu on 3/30/2016.
  */
 public class TwoFragment extends Fragment {
+
+    private List<Person> persons;
+    private RecyclerView rv;
+
+
     public TwoFragment() {
 
     }
@@ -23,6 +33,28 @@ public class TwoFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View rootView = inflater.inflate(R.layout.recyclerview_activity, container, false);
+        rv = (RecyclerView) rootView.findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        initializeData();
+        initializeAdapter();
+
+        return rootView;
+    }
+
+    private void initializeData() {
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+
+    private void initializeAdapter() {
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
     }
 }
